5f8a889920e2f8f2bcde9ecf376fcd674bf1a4cb
Kufre | DevSecOps <kufre@businessflaws.com>
2024-12-07 16:25:41 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -38,9 +38,9 @@ function printHeader(title) {
 
     const vpnStatus = await isVpn(ipAddress);
     if (vpnStatus) {
-        res.send("This IP address is using a VPN. ");
+        res.send(ipAddress + "This IP address is using a VPN. ");
     } else {
-        res.send("This IP address is not using a VPN. ");
+        res.send(ipAddress + "This IP address is not using a VPN. ");
     }
 })()
 });
